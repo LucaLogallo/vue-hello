b6467fe4142c59c0ef2edae1f4b1429b575fc294
LucaLogallo <lucalogallo1998@gmail.com>
2021-05-04 16:20:18 +0200

    tasto compare/scompare testo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,29 @@ const app = new Vue ({
   el : '#app',
   data:{
     messaggioH1: "messaggio da stampare nell'h1",
+    messaggio2H1: "altro messaggio",
+    classeActive: "testoNascosto",
     classiH1:"testoH1",
-    miaClasse:"testoH1"
+    miaClasse:"testoH1",
+    isActive : false
   },
-  method:{
+  methods:{
     cambiaClasse(classe){
-      this.miaClasse = classe;
+      console.log('ciao');
+      this.miaClasse = "testoH1";
+      this.miaClasse += classe;
+    },
+    attiva(classe){
+      if(this.isActive === false){
+        this.isActive = !this.isActive;
+        this.classeActive += classe;
+        console.log("attiva");
+      }else{
+        this.classeActive = "testoNascosto";
+        this.isActive = false;
+        console.log("disattiva");
+      };
+      console.log('ciao');
     }
   }
 });
